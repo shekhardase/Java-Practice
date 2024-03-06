4d61b90472321a3ad6a8117949ffbdb2973b5d4c
Shekhar <115777486+shekhardase@users.noreply.github.com>
2024-03-06 15:25:57 +0530

    added code

--- TwoDArray.java
+++ TwoDArray.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 public class TwoDArray {
     public static void main(String[] args) {
         int[][] matrix = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
         };
 
         System.out.println("Original Matrix:");
